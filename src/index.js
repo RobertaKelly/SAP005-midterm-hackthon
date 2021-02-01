50d15602d3567c1ecf71dfe2eaffb4537d691e4f
elis-ctrl <elis_emb@hotmail.com>
2021-02-01 15:55:51 -0300

    exclusão de pastas

--- src/index.js
+++ src/index.js
@@ -16,3 +16,26 @@ getPoster().then(posterPath => {
   `;
   console.log(imgSrc);
 })
+import movieGenres from "./genres.js"
+
+let imgBaseUrl = "https://image.tmdb.org/t/p/w200";
+let imgSrc = "";
+
+const getPoster = () => {
+  return fetch(movieGenres.actionNfx)
+    .then(response => response.json())
+    .then(json => json.results);
+}
+
+getPoster().then(moviesList => {
+  console.log(moviesList)
+
+  for (let movie of moviesList) {
+    console.log(movie.poster_path)
+  }
+  // imgSrc = imgBaseUrl + posterPath;
+  // document.getElementById("root").innerHTML = `
+  // <img src="${imgSrc}">
+  // `;
+  // console.log(imgSrc);
+})
