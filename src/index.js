03b93e89f59dfd333616655fc10f094244a7b6de
elis-ctrl <elis_emb@hotmail.com>
2021-02-02 10:28:34 -0300

    testes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
 import movieGenres from "./genres.js"
 
-let imgBaseUrl = "https://image.tmdb.org/t/p/w200";
-let imgSrc = "";
-let posters = "";
+let imgBaseUrl = 'https://image.tmdb.org/t/p/w200';
+let imgSrc = '';
+let posters = '';
 
-const getPoster = () => {
-  return fetch(movieGenres.animationNfx)
-    .then(response => response.json())
-    .then(json => json.results)
-}
+// const getPoster = () => {
+//   return fetch(movieGenres.animationNfx)
+//     .then(response => response.json())
+//     .then(json => json.results)
+// }
 
-getPoster().then(moviesList => {
-  console.log(moviesList);
-  for (let movie of moviesList) {
-    console.log(movie.poster_path);
-    imgSrc = imgBaseUrl + movie.poster_path;
-    console.log(imgSrc);
-    posters += `
-      <img src="${imgSrc}">
-    `;
-  }
-  document.getElementById("root").innerHTML = posters;
-})
-document.querySelector('#generos').onchange = () => {
- const filterGender = document.querySelector('#generos').value;
+// getPoster().then(moviesList => {
+//   console.log(moviesList);
+//   for (let movie of moviesList) {
+//     console.log(movie.poster_path);
+//     imgSrc = imgBaseUrl + movie.poster_path;
+//     console.log(imgSrc);
+//     posters += `
+//       <img src='${imgSrc}'>
+//     `;
+//   }
+//   document.getElementById('root').innerHTML = posters;
+// })
+//para pegar os filtros no carrossel
+
+document.getElementById('generos').onchange = () => {
+  // const gender = document.getElementById('generos').value;
+  const getGender = () => {
+      return fetch(movieGenres.actionNfx)
+      .then(response => response.json())
+      .then(json => json.results)
+    }
+  getGender().then(moviesList => {
+    console.log(moviesList);
+    for (let movie of moviesList) {
+      console.log(movie.poster_path);
+      imgSrc = imgBaseUrl + movie.poster_path;
+      console.log(imgSrc);
+      posters += `
+        <img src='${imgSrc}'>
+      `;
+    }
+    document.getElementById('root').innerHTML = posters;
+  })
 }
